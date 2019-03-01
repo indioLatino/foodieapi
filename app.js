93e6d13bece903ce6cbdd6d173018be4c96d0223
Lempira <rodolfogalotejada@gmail.com>
2019-03-01 18:27:57 +0100

    Environment configuration

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const item = require('./routes/item.route'); // Imports routes for the products
+// initialize our express app
+// config variables
+const config = require('./config/config.js');
+const app = express();
+app.get('/', (req, res) => {
+    res.json(global.gConfig);
+});
 // Set up mongoose connection
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://localhost/foodie';
+process.env.NODE_ENV = 'development';
+let dev_db_url = global.gConfig.database;
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
-/*mongoose.connect("mongodb://localhost:27017/foodie", {
-    "user": "foodieMongoUser",
-    "pass": "15091821",
-    "useMongoClient": true,
-    "useNewUrlParser": true
-});*/
 mongoose.connect(mongoDB,{ useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// initialize our express app
-const app = express();
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(function(req, res, next) {
@@ -27,7 +29,8 @@ app.use(function(req, res, next) {
   next();
  });
 app.use('/item', item);
-let port = 1234;
+let port = global.gConfig.node_port;
+let app_name=global.gConfig.app_name;
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log(app_name+'is up and listening on port numner ' + port);
 });
